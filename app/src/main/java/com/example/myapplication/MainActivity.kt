152bbbfb46a5748d9d639687a03833392c2cee83
kenneth <ken.jav550@gmail.com>
2025-08-29 02:16:51 -0500

    act 5 Art Compose

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -4,17 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.myapplication.ui.theme.MyApplicationTheme
 import androidx.compose.material3.Surface
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import androidx.compose.foundation.layout.Column
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,11 +25,8 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MyApplicationTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(modifier = Modifier.fillMaxSize()) {
+                    GreetingImage()
                 }
             }
         }
@@ -34,19 +34,47 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Surface(color = Color.Cyan) {
-        Text(
-            text = "Hello $name!",
-            modifier = modifier.padding(24.dp)
+fun GreetingText(tittle: String, parrOne: String, parrTwo: String, modifier: Modifier = Modifier) {
+    Text(
+        text = tittle,
+        fontSize = 24.sp,
+        modifier = modifier.padding(16.dp)
+    )
+    Text(
+        text = parrOne,
+        modifier = Modifier.padding(
+            start = 16.dp,
+            end = 16.dp
         )
+    )
+    Text(
+        text = parrTwo,
+        modifier = modifier.padding(16.dp)
+    )
+}
+
+@Composable
+fun GreetingImage(modifier: Modifier = Modifier) {
+    val image = painterResource(R.drawable.bg_compose_background)
+    Column {
+        Image(
+            painter = image,
+            contentDescription = null
+        )
+        Column {
+            GreetingText(tittle = stringResource(R.string.tittle), parrOne = stringResource(
+                R.string.parr_one
+            ), parrTwo = stringResource(R.string.parr_two)
+            )
+        }
     }
+
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     MyApplicationTheme {
-        Greeting("DSM-G3")
+        GreetingImage()
     }
 }
